c00bf6a5e4c44a7dc78e2990f24d15b4839c7c91
funeswen001 <funeswen001@warren.k12.in.us>
2025-08-29 08:16:37 -0400

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,96 @@
+"""
+Assignment 1: Calculator Program
+Student Name: Wendolly Funes
+Date: 08/29/25
+
+This program demonstrates the use of arithmetic operators in Python.
+"""
+
+# Store two numbers in variables
+# Remember: We learned about variables in the previous lesson
+number1 = 75
+number2 = 40
+
+# TODO: Perform addition and store the result in a variable
+# Hint: Use the + operator
+oranges = number1 + number2
+print(oranges)
+print(number1)
+print(number2)
+total_fruit = number1 + number2
+print("Total fruit:", total_fruit)
+
+# TODO: Print the addition result
+# Hint: Use the print() function we learned in previous lessons
+total_fruit = number1 + number2
+print("Total fruit:", total_fruit)
+
+# TODO: Perform subtraction and store the result
+Apples_in_a_tree = 50
+Apples_fall_in_the_ground = 12
+Apples_still_in_the_tree = Apples_in_a_tree - Apples_fall_in_the_ground
+print("Apples_still_in_the_tree:", Apples_still_in_the_tree)
+
+# TODO: Print the subtraction result
+Apples_still_in_the_tree = Apples_in_a_tree - Apples_fall_in_the_ground
+print("Apples_still_in_the_tree:", Apples_still_in_the_tree)
+
+# TODO: Perform multiplication and store the result
+packages = 30
+items_per_packages = 24
+total_items = packages * items_per_packages
+print("Total items:", total_items)
+
+# TODO: Print the multiplication result
+total_items = packages * items_per_packages
+print("Total items:", total_items)
+
+# TODO: Perform division and store the result
+total_spaces = 25
+number_of_workers = 350
+number_of_workers_per_space= number_of_workers / total_spaces
+print("number of workers per space:", number_of_workers_per_space)
+
+# TODO: Print the division result
+number_of_workers_per_space= number_of_workers / total_spaces
+print("number of workers per space:", number_of_workers_per_space)
+
+
+# TODO: Perform floor division and store the result
+total_toys = 70
+childrends  = 25
+toys_per_child= total_toys // childrends
+print("toys per child:", toys_per_child, )
+
+# TODO: Print the floor division result
+toys_per_child= total_toys // childrends
+print("toys per child:", toys_per_child, )
+
+# TODO: Perform modulus operation and store the result
+price = 100
+new_price = 120
+price_increase = new_price % price
+print("price increase:", price_increase)
+
+# TODO: Print the modulus result
+price_increase = new_price % price
+print("price increase:", price_increase)
+
+# TODO: Perform exponentiation (number1 to the power of number2)
+base =300
+exponent = 7
+result = base ** exponent
+print("300 to the power of 7:", result)
+
+# TODO: Print the exponentiation result
+result = base ** exponent
+print("300 to the power of 7:", result)
+
+
+# BONUS CHALLENGE:
+# Create a more complex calculation using multiple operators
+# For example: (number1 + 10) * number2 - 5
+# TODO: Create and print a complex calculation
+complex_calc = 547 + 28 ** 9 * 70 - 200 / 10
+print("Complex calculation:", complex_calc)
+
